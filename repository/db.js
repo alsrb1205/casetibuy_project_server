fd4f6bed80ea00bf14c16667ce85e420ef846920
alsrb1205 <alsrb1205@gmail.com>
2025-04-01 14:32:34 +0900

    서버파일

---
+++ repository/db.js
@@ -0,0 +1,15 @@
+/**
+ * 1. DB 연동 라이브러리 호출 - DB서버주소, user, password, port 
+ */
+import mysql from 'mysql2';
+
+const pool = mysql.createPool({
+    host: 'casetibuy-db.c1uagmqqwxda.ap-northeast-2.rds.amazonaws.com',
+    port: 3306,
+    user: 'admin',
+    password: 'casetibuy!1344',
+    database: 'casetibuy',
+    charset: 'utf8mb4'
+});
+
+export const db = pool.promise();
