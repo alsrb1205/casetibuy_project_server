53610e2a7a16b7dcaff325011efcf3ce869ebc15
alsrb1205 <alsrb1205@gmail.com>
2025-04-01 16:40:25 +0900

    asdsad

--- controller/orderController.js
+++ controller/orderController.js
@@ -2,72 +2,69 @@ import { db } from "../repository/db.js";
 import * as orderRepository from "../repository/orderRepository.js";
 
 // 결제 완료 후 실행되는 주문 생성 함수
-export const createOrder = async (req, res) => {  
-  
+export const createOrder = async (req, res) => {
+
   const { member_id, total_price, payment_method, zipcode, address, detail_address, cartItems } = req.body;
-  console.log(cartItems);
-  console.log(req.body);
-  
 
-    // if (!member_id || !total_price || !payment_method || !zipcode || !address || !detail_address || !cartItems) {
-    //     return res.status(400).json({ message: "필수 정보를 입력해주세요." });
-    // }
+  if (!member_id || !total_price || !payment_method || !zipcode || !address || !detail_address || !cartItems) {
+    return res.status(400).json({ message: "필수 정보를 입력해주세요." });
+  }
 
-    // const connection = await db.getConnection();
-    // try {
-    //     await connection.beginTransaction();
+  const connection = await db.getConnection();
+  try {
+    await connection.beginTransaction();
 
-    //     const orderSql = `
-    //                         INSERT INTO casetibuy_order (member_id, total_price, payment_method, zipcode, address, detail_address)
-    //                         VALUES (?, ?, ?, ?, ?, ?)`;
-    //     const [orderResult] = await connection.execute(orderSql, [
-    //         member_id,
-    //         total_price,
-    //         payment_method,
-    //         zipcode,
-    //         address,
-    //         detail_address,
-    //     ]);
+    const orderSql = `
+                            INSERT INTO casetibuy_order (member_id, total_price, payment_method, zipcode, address, detail_address)
+                            VALUES (?, ?, ?, ?, ?, ?)`;
+    const [orderResult] = await connection.execute(orderSql, [
+      member_id,
+      total_price,
+      payment_method,
+      zipcode,
+      address,
+      detail_address,
+    ]);
 
-    //     const order_id = orderResult.insertId;
+    const order_id = orderResult.insertId;
 
-    //     const orderDetailSql = `
-    //                                 INSERT INTO casetibuy_order_detail (order_id, product_id, product_name, qty, unit_price, kinds, color, case_type, product_image)
-    //                                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+    const orderDetailSql = `
+                                    INSERT INTO casetibuy_order_detail (order_id, product_id, product_name, qty, unit_price, kinds, color, case_type, product_image)
+                                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
 
-    //     for (const item of cartItems) {
-    //         await connection.execute(orderDetailSql, [
-    //             order_id,
-    //             item.product_id,
-    //             item.product_name,
-    //             item.qty,
-    //             item.unit_price,
-    //             item.kinds,
-    //             item.color,
-    //             item.case_type,
-    //             item.product_image,
-    //         ]);
-    //     }
+    for (const item of cartItems) {
+      await connection.execute(orderDetailSql, [
+        order_id,
+        item.product_id,
+        item.product_name,
+        item.qty,
+        item.unit_price,
+        item.kinds,
+        item.color,
+        item.case_type,
+        item.product_image,
+      ]);
+    }
 
-    //     const deleteCartSql = `DELETE FROM casetibuy_cart WHERE id = ?`;
-    //     await connection.execute(deleteCartSql, [member_id]);
+    const deleteCartSql = `DELETE FROM casetibuy_cart WHERE id = ?`;
+    await connection.execute(deleteCartSql, [member_id]);
 
-    //     await connection.commit();
-    //     res.status(201).json({ message: "주문이 성공적으로 저장되었습니다.", order_id });
-    // } catch (error) {
-    //     await connection.rollback();
-    //     console.error("주문 생성 중 오류 발생:", error);
-    //     res.status(500).json({ message: "주문 저장 중 오류 발생" });
-    // } finally {
-    //     connection.release();
-    // }
+    await connection.commit();
+    res.status(201).json({ message: "주문이 성공적으로 저장되었습니다.", order_id });
+  } catch (error) {
+    await connection.rollback();
+    console.error("주문 생성 중 오류 발생:", error);
+    res.status(500).json({ message: "주문 저장 중 오류 발생" });
+  } finally {
+    connection.release();
+  }
 };
 
 
 
 
 export const getOrders = async (req, res) => {
-  
+
   try {
     const { memberId } = req.body;
     if (!memberId) {
